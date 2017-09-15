0870cfd34a983672d6a59676788eba9bf5fbb782
robin <robin@Robin-Workstation>
2017-09-15 14:02:23 +0200

    popopo

--- www/js/sound.js
+++ www/js/sound.js
@@ -1,6 +1,8 @@
 addListeners();
 
 function addListeners(){
+	console.log("add listeners")
+	document.addEventListener("deviceready", onDeviceReady, false);
 	var playSoundButtons = document.getElementsByClassName('playSound');
 
 	for(var i=0; i<playSoundButtons.length; i++)
@@ -8,10 +10,15 @@ function addListeners(){
 		var btn = playSoundButtons[i];
 		btn.addEventListener("click", playSound, false); 
 	}
+	console.log("finished add")
 }
 
 function playSound(){
 	var soundId = this.dataset.sound;
 	var audio = document.getElementById(soundId);
 	return audio.play();
+}
+
+function onDeviceReady() {
+
 }
